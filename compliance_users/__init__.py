f64350001e570c1258b85335bffdc09ed343a028
Raphael Hagen <norlandrhagen@gmail.com>
2024-12-19 13:59:30 -0500

    Update compliance_users/__init__.py
    Co-authored-by: Anderson Banihirwe <13301940+andersy005@users.noreply.github.com>

--- compliance_users/__init__.py
+++ compliance_users/__init__.py
@@ -1,6 +1,6 @@
 from importlib.metadata import version as _version
 
 try:
-    __version__ = _version("{{ cookiecutter.project_slug }}")
+    __version__ = _version("carbonplan_compliance_users")
 except Exception:
     __version__ = "9999"
