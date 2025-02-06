69468d43a5ca8cf19dd1d20969c18494ba841ef5
Shane Loeffler <Shane98c@users.noreply.github.com>
2025-02-06 17:00:58 -0600

    use script

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import PlausibleProvider from 'next-plausible'
+import Script from 'next/script'
 import { ThemeProvider } from 'theme-ui'
 import { MDXProvider, useMDXComponents } from '@mdx-js/react'
 import { useThemedStylesWithMdx } from '@theme-ui/mdx'
@@ -10,6 +10,16 @@ import useStore from '../components/use-store'
 import Header from '../components/header'
 import Layout from '../components/layout'
 import Info from '../components/info'
+
+const Analytics = () =>
+  process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? (
+    <Script
+      strategy='lazyOnload'
+      data-domain='carbonplan.org'
+      data-api='https://carbonplan.org/proxy/api/event'
+      src='https://carbonplan.org/js/script.file-downloads.outbound-links.js'
+    />
+  ) : null
 
 const App = ({ Component, pageProps, router }) => {
   const data = useStore((state) => state.data)
@@ -22,28 +32,26 @@ const App = ({ Component, pageProps, router }) => {
 
   if (router.route === '/404') {
     return (
-      <PlausibleProvider domain='carbonplan.org'>
-        <ThemeProvider theme={theme}>
-          <MDXProvider components={components}>
-            <Component {...pageProps} />
-          </MDXProvider>
-        </ThemeProvider>
-      </PlausibleProvider>
+      <ThemeProvider theme={theme}>
+        <Analytics />
+        <MDXProvider components={components}>
+          <Component {...pageProps} />
+        </MDXProvider>
+      </ThemeProvider>
     )
   }
 
   return (
-    <PlausibleProvider domain='carbonplan.org'>
-      <ThemeProvider theme={theme}>
-        <MDXProvider components={components}>
-          <Layout>
-            <Header />
-            <Component {...pageProps} />
-            <Info />
-          </Layout>
-        </MDXProvider>
-      </ThemeProvider>
-    </PlausibleProvider>
+    <ThemeProvider theme={theme}>
+      <Analytics />
+      <MDXProvider components={components}>
+        <Layout>
+          <Header />
+          <Component {...pageProps} />
+          <Info />
+        </Layout>
+      </MDXProvider>
+    </ThemeProvider>
   )
 }
 
